2a47f7fa1bd0878b25cdfef02cec29a2ade1af72
Sara Laffin <sarala@umich.edu>
2020-03-07 02:55:18 -0500

    add conditional rendering if more than 1 country present

--- src/PeakContent/PeakContent.js
+++ src/PeakContent/PeakContent.js
@@ -1,43 +1,49 @@
 import React from "react";
 
 function PeakContent(props) {
-  if (props.Countries.length > 1) {
-    return (
-      <div className="d-flex flex-column align-items-center">
-        <h1>{props.Name}</h1>
-        <p>
-          <span>Height: </span>
-          {props.Height} meters
-        </p>
-        <p>
-          <span>Prominence: </span>
-          {props.Prominence} meters
-        </p>
-        <p>
-          <span>Countries: </span>
-          {props.Countries.join(", ")}
-        </p>
-        <p>
-          <span>Happiness ranking: </span>
-          {props.Happiness[0].Overallrank}, {props.Happiness[1].Overallrank}
-        </p>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <h1>{props.Name}</h1>
-        <p>
-          <span>Country: </span>
-          {props.Countries}
-        </p>
-        <p>
-          <span>Happiness ranking: </span>
-          {props.Happiness[0].Overallrank}
-        </p>
-      </div>
-    );
-  }
+  let multipleCountries = () => {
+    if (props.Countries.length > 1) {
+      return (
+        <>
+          <p>
+            <span>Countries: </span>
+            {props.Countries.join(", ")}
+          </p>
+          <p>
+            <span>Happiness ranking: </span>
+            {props.Happiness[0].Overallrank}, {props.Happiness[1].Overallrank}
+          </p>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <p>
+            <span>Country: </span>
+            {props.Countries}
+          </p>
+          <p>
+            <span>Happiness ranking: </span>
+            {props.Happiness[0].Overallrank}
+          </p>
+        </>
+      );
+    }
+  };
+  return (
+    <div className="d-flex flex-column align-items-center">
+      <h1>{props.Name}</h1>
+      <p>
+        <span>Height: </span>
+        {props.Height} meters
+      </p>
+      <p>
+        <span>Prominence: </span>
+        {props.Prominence} meters
+      </p>
+      {multipleCountries()}
+    </div>
+  );
 }
 
 export default PeakContent;
